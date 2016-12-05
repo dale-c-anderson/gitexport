e149f928363436616336a0febf8316f0f68c5e8c
Dale Anderson <danderson@acromediainc.com>
2016-12-05 13:30:35 -0800

    Make remote deploy less chatty; source of errors less ambiguous

--- deploy-local.sh
+++ deploy-local.sh
@@ -1,17 +1,23 @@
 #!/bin/bash
 
+################################################################################
+#
+# If you change this file, you also need to update
+# REMOTE_DEPLOY_TOOL_CHECKSUM in gitexport-deploy.sh
+#
+################################################################################
+
 if [ $# -ne 3 ]; then
   echo "For deploying exported / archived git packages (or other prepared bundles) locally once they have been pushed via ssh"
   echo "Requires sudo privileges."
   echo "Moves the deploy file to the target dir, sets the correct owner, extracts the bundle, and then removes it."
-  echo "Usage:   $(basename "$0") <deployfile.tar.gz> </full/path/to/dest/dir> <owner>"
-  echo "Example: $(basename "$0") 'telecomstorebox-LATEST.tgz' '/home/acro/apps/telecomstorebox' 'acro'"
+  echo "Usage:   $(basename "$0") ./bundle-to-unpack.tar.gz /full/path/to/dest/dir/ owner"
   exit 0
 fi
 
 deployfile="$1"
 if [ ! -f "$deployfile" ]; then
-  echo "Deploy file does not exist: $deployfile"
+  echo "$(basename "$0") Err: Deploy file does not exist: $deployfile"
   exit 1
 fi
 
@@ -21,50 +27,37 @@ if [[ "$deployfile" == *.tar.gz ]]; then
 elif [[ "$deployfile" == *.tgz ]]; then
   IS_TGZ=1
 else
-  echo "The deploy file is expected to be in .tgz or .tar.gz format."
+  echo "$(basename "$0") Err: The deploy file is expected to be in .tgz or .tar.gz format."
   exit 1
 fi
 
 deploydir="$2"
-if [ ! -d "$deploydir" ]; then
-  echo "Oops  - deploydir doesnt exist: $deploydir"
+if ! sudo test -d "$deploydir"; then
+  echo "$(basename "$0") Err: deploy dir doesnt exist: $deploydir"
   exit 1
 fi
 
 deployuser="$3"
 getent passwd "$deployuser" >/dev/null 2>&1 || {
-  echo "It doesn't look like the user exists: $deployuser"
+  echo "$(basename "$0") Err: user doesn't exist: $deployuser"
   exit 1
 }
 
-echo -n "Moving $deployfile to $deploydir/ ... "
-sudo mv "$deployfile" "$deploydir/" || {
-  echo "Could not move deploy file to target dir. Aborting."
+## Moving and chowning isn't necessary for tar to work, but when we unpack it as the deploy user,
+## they may not have permission to read it or get at it where we uploaded it to.
+sudo mv -v "$deployfile" "$deploydir/" || {
+  echo "$(basename "$0") Err: move failed"
   exit 1
 }
-echo "OK"
-
-# @TODO: Is this step actually necessary? I think running tar as the specified user is what sets permissions as the files are being extracted.
-echo -n "Setting ownership of $deployfile to $deployuser ... "
 sudo chown "$deployuser": "$deploydir/$deployfile"|| {
-  echo "Could not chown deploy file. Aborting."
+  echo "$(basename "$0") Err: Could not chown deploy file. Aborting."
   exit 1
 }
-echo "OK"
-
-# @TODO: Moving the tar file isn't necessary - you can use the -C switch to extract contents to a different directory.
-echo "Extracting $deployfile... "
-sudo -u "$deployuser" -H sh -c "cd $deploydir && tar xzfv $deployfile" || {
-  echo "Could not extract deploy file to target. Deploy failed."
+sudo -u "$deployuser" -H sh -c "cd '$deploydir' && tar xzfv '$deployfile'" || {
+  echo "$(basename "$0") Err: Could not extract deploy file to target. Deploy failed."
   exit 1
 }
-echo "OK"
 
-echo -n "Removing $deployfile... "
 sudo rm "$deploydir/$deployfile" || {
-  echo "Could not remove $deployfile. You'll need to remove it manually."
+  echo "$(basename "$0") Warn: Could not remove '$deployfile'. You'll need to remove it manually."
 }
-echo "OK"
-
-echo "All done."
-exit 0
